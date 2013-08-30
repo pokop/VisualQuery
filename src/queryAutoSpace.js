cd3d4840016f60740768fc06f1ce3a41015d6473
or virnik <or.virnik@gmail.com>
2013-08-31 01:04:18 +0300

    Resolves #1: Disable auto-space after broken value.

--- src/queryAutoSpace.js
+++ src/queryAutoSpace.js
@@ -12,13 +12,15 @@
 		return str.search(prefix) === 0;
 	}
 
+	var BROKEN_VALUE_ERROR = 'error broken-value';
+	
 	function buildKeymap() {
 		return {
 			name : "queryAutoSpace",
 			Space: function (cm) {
 				var cur = cm.getCursor(), token = cm.getTokenAt(cur), line = cm.getLine(cur.line);
 				
-				if (cur.ch === token.end && token.type !== null) {
+				if (cur.ch === token.end && token.type !== null && token.type !== BROKEN_VALUE_ERROR) {
 					var nextToken = cm.getTokenAt(CodeMirror.Pos(cur.line, cur.ch + 1)), start = cur, end, literal = ',';
 						
 					if (line[cur.ch] === ';')
